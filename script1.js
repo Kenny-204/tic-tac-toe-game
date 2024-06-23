cf6c03bd37653c81b5ce1d2fbfab1f4ed4583433
Kenny-204 <giwatwins@gmail.com>
2024-06-23 18:12:17 +0100

    initial commit

---
+++ script1.js
@@ -0,0 +1,53 @@
+'use strict ';
+const message = document.querySelector('.p21');
+const box = document.querySelectorAll('.item');
+const newGame = document.querySelector('.new');
+activePlayer = 0;
+playing = true;
+function init() {
+    for (let i = 0; i < box.length; i++) {
+        box[i].textContent = '';
+    }
+    message.textContent = ' X goes First...';
+    playing = true;
+}
+;
+// console.log(box)
+for (let i = 0; i < box.length; i++) {
+    box[i].addEventListener('click', function () {
+
+
+
+
+        box[i].textContent = 'X';
+        activePlayer = 1; // Switch to computer's turn
+
+        // Computer's move
+        let rand = Math.trunc(Math.random() * 9);
+        while (box[rand].textContent != '') {
+            rand = Math.trunc(Math.random() * 9);
+        }
+        if (box[rand].textContent == '') {
+            box[rand].textContent = 'O';
+            console.log(rand);
+        }
+        activePlayer = 0; // Switch back to player's turn        }
+        if (box[i].textContent == '' && (activePlayer == 1) && playing) {
+           
+
+        }
+
+        if (((box[0].textContent == 'X') && (box[1].textContent == 'X') && box[2].textContent == 'X') || ((box[3].textContent == 'X') && (box[4].textContent == 'X') && box[5].textContent == 'X') || ((box[6].textContent == 'X') && (box[7].textContent == 'X') && box[8].textContent == 'X') || ((box[0].textContent == 'X') && (box[3].textContent == 'X') && box[6].textContent == 'X') || ((box[1].textContent == 'X') && (box[4].textContent == 'X') && box[7].textContent == 'X') || ((box[2].textContent == 'X') && (box[5].textContent == 'X') && box[8].textContent == 'X') || ((box[0].textContent == 'X') && (box[4].textContent == 'X') && box[8].textContent == 'X') || ((box[2].textContent == 'X') && (box[4].textContent == 'X') && box[6].textContent == 'X')) {
+            message.textContent = 'X wins';
+
+            playing = false;
+        }
+        else if (((box[0].textContent == 'O') && (box[1].textContent == 'O') && box[2].textContent == 'O') || ((box[3].textContent == 'O') && (box[4].textContent == 'O') && box[5].textContent == 'O') || ((box[6].textContent == 'O') && (box[7].textContent == 'O') && box[8].textContent == 'O') || ((box[0].textContent == 'O') && (box[3].textContent == 'O') && box[6].textContent == 'O') || ((box[1].textContent == 'O') && (box[4].textContent == 'O') && box[7].textContent == 'O') || ((box[2].textContent == 'O') && (box[5].textContent == 'O') && box[8].textContent == 'O') || ((box[0].textContent == 'O') && (box[4].textContent == 'O') && box[8].textContent == 'O') || ((box[2].textContent == 'O') && (box[4].textContent == 'O') && box[6].textContent == 'O')) {
+            message.textContent = 'O wins';
+            playing = false;
+        }
+        // else {message.textContent = 'It`s a draw'}
+    });
+}
+;
+newGame.addEventListener('click', init);
